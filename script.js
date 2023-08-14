c81128f404838f9cb3bc68e3482dec541faf3c2f
Adi6583 <92431471+Adi6583@users.noreply.github.com>
2023-08-14 19:53:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Capital Efficient Options Trading</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h1 {
+            text-align: center;
+        }
+        h2 {
+            text-align: center;
+        }
+        .button-container {
+            display: flex;
+            justify-content: center;
+        }
+        .button {
+            background-color: white;
+            color: black;
+            padding: 10px 20px;
+            margin: 10px;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+        .button:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .icon-container {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
+</head>
+<body>
+    <div class="icon-container">
+        <a href="#"><img src="discord.png" alt="Discord" width="32" height="32"></a>
+        <a href="#"><img src="twitter.png" alt="Twitter" width="32" height="32"></a>
+    </div>
+    <h1>Capital Efficient Options Trading</h1>
+    <h2>One click multi leg options trades, capital efficient strategies, automatically hedged risks, and more.</h2>
+    <div class="button-container">
+        <a href="#" class="button">Built on Lyra Synthetix</a>
+        <a href="#" class="button">Available on Arbitrum OP</a>
+    </div>
+    <div class="icon-container">
+        <a href="#" class="button">Launch App</a>
+    </div>
+</body>
+</html>
